37059f0e2b63e0e3faf9a0b9640cfde4c3448e42
Don Boyle <donboyle@ymail.com>
2019-03-15 13:37:04 -0500

    worked in a max and min  seconds, randomized time on load, implemented index list to show current index of randomized list in log. reworked log to be 1 line. Padded said line and really cleaned output

--- EdgeAutoPoints/Program.cs
+++ EdgeAutoPoints/Program.cs
@@ -3,33 +3,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel.Syndication;
+using System.Threading;
 using System.Xml;
 
 namespace EdgeAutoPoints
 {
-    class Program
+    internal class Program
     {
-        private static int sleepTimer = 4000;
+        public class SyndicationItemWIndex
+        {
+            public int Index { get; set; }
+            public SyndicationItem Item { get; set; }
+        }
 
+        private static readonly int minSleepTimerSeconds = 1;
+        private static readonly int maxSleepTimerSeconds = 4;
+        
         public static void Main()
         {
             var items = getSyndicationItems();
 
             var answer = "";
-
             Console.WriteLine("Start earning points on (" + items.Count + " pages) in Edge? (Yes/No)");
-
             answer = Console.ReadLine();
-
             if (answer == "Yes" || answer == "yes" || answer == "Y" || answer == "y")
             {
                 //randomize
-                List<SyndicationItem> shuffledItems = new List<SyndicationItem>();
+                var shuffledItems = new List<SyndicationItemWIndex>();
                 shuffledItems.AddRange(items.OrderBy(a => Guid.NewGuid()).ToList());
 
                 RunFeedProcess(shuffledItems);
             }
-        
+
             else
             {
                 Console.WriteLine("Why did you even open this...?");
@@ -39,56 +44,76 @@ namespace EdgeAutoPoints
             Console.ReadKey();
         }
 
-        private static List<SyndicationItem> getSyndicationItems()
+        private static List<SyndicationItemWIndex> addIndexToList(List<SyndicationItem> items) 
         {
-            var TulsaNewsUrl = "http://www.newson6.com/category/208401/newson6com-news-rss?clienttype=rss"; //Tulsa Local News
+            return items.Select((item, index) => new SyndicationItemWIndex { Index = index, Item = item }).ToList();
+        }
+
+        private static List<SyndicationItemWIndex> getSyndicationItems()
+        {
+            var TulsaNewsUrl =
+                "http://www.newson6.com/category/208401/newson6com-news-rss?clienttype=rss"; //Tulsa Local News
             var Url = "http://www.wsj.com/xml/rss/3_7455.xml"; //The Wall Street Journal: "Technology: What's News"
             var bbcUrl = @"http://feeds.bbci.co.uk/news/rss.xml?edition=uk";
             var bbcUSCanada = @"http://feeds.bbci.co.uk/news/rss.xml?edition=us";
             var bbcRestWorld = @"http://feeds.bbci.co.uk/news/rss.xml?edition=int";
-            
+
             //get all items
-            List<SyndicationItem> items = new List<SyndicationItem>();
+            var items = new List<SyndicationItem>();
             items.AddRange(getSyndicationFeed(TulsaNewsUrl).Items.ToList());
             items.AddRange(getSyndicationFeed(bbcUrl).Items.ToList());
             items.AddRange(getSyndicationFeed(bbcUSCanada).Items.ToList());
             items.AddRange(getSyndicationFeed(bbcRestWorld).Items.ToList());
-            return items;
+
+            return addIndexToList(items);
         }
 
         private static SyndicationFeed getSyndicationFeed(string url)
         {
-            XmlReader reader = XmlReader.Create(url);
-            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            var reader = XmlReader.Create(url);
+            var feed = SyndicationFeed.Load(reader);
             return feed;
         }
 
-        private static void RunFeedProcess(List<SyndicationItem> items)
+        private static void RunFeedProcess(List<SyndicationItemWIndex> items)
         {
-            for (int i = 1; i < items.Count; i++)
+            var totalSeconds = items.Count * maxSleepTimerSeconds;
+            var lapsedSeconds = 0;
+            for (var i = 1; i < items.Count; i++)
             {
-               var item = items[i];
+                var randSleepTimer = randNumber(maxSleepTimerSeconds, minSleepTimerSeconds);
+                lapsedSeconds += randSleepTimer;
+                totalSeconds -= randSleepTimer;
+                var item = items[i];
 
-                Console.WriteLine(i + "/" + items.Count + " ---> " + item.Title.Text);
-                var totalSeconds = (items.Count * sleepTimer) / 1000;
-                var currentSeconds = (i * sleepTimer) / 1000;
+                var calcTimeRemaining =
+                    "T-" + (totalSeconds - lapsedSeconds).ToString().PadLeft(totalSeconds.ToString().Length);
+                var calcRunning = " Running:" + i.ToString().PadLeft(items.Count.ToString().Length) + "/" + items.Count;
+                var calcCurrentIndex = " index[" + item.Index.ToString().PadLeft(items.Count.ToString().Length) + "]";
+                var calcTitle = " search: '" + item.Item.Title.Text + "'";
 
-                Console.WriteLine((totalSeconds - currentSeconds) + "s || " + ((totalSeconds - currentSeconds) / 60) + "m remaining");
-                
-                System.Threading.Thread.Sleep(sleepTimer);
+                Console.WriteLine(calcTimeRemaining
+                               + calcRunning
+                               + calcCurrentIndex
+                               + calcTitle);
+                Thread.Sleep(randSleepTimer * 1000);
 
-                var search = Process.Start("microsoft-edge:https://www.bing.com/search?q=" + item.Title.Text + "");
-                search?.WaitForExit();
+                //var search = Process.Start("microsoft-edge:https://www.bing.com/search?q=" + item.Item.Title.Text + "");
+                //search?.WaitForExit();
             }
+
             KillIt();
+        }
+
+        private static int randNumber(int maxInt = 4, int startInt = 0)
+        {
+            var r = new Random();
+            return r.Next(startInt, maxInt); //for ints
         }
 
         private static void KillIt()
         {
-            foreach (var process in Process.GetProcessesByName("MicrosoftEdgeCP"))
-            {
-                process.Kill();
-            }
+            foreach (var process in Process.GetProcessesByName("MicrosoftEdgeCP")) process.Kill();
         }
     }
 }
